503ee8d0f40585188f0ec109836c6f801a4f1a86
khaitb <khaikhai1223@gmail.com>
2022-11-01 03:15:49 +0700

    init project.

---
+++ Login_Fb/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Login_Fb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rdDataImport = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.proxy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.note = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginEmailPassToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginEmailPass2FAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdDataImport
+            // 
+            this.rdDataImport.Location = new System.Drawing.Point(13, 13);
+            this.rdDataImport.Name = "rdDataImport";
+            this.rdDataImport.Size = new System.Drawing.Size(416, 145);
+            this.rdDataImport.TabIndex = 0;
+            this.rdDataImport.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stt,
+            this.email,
+            this.pass,
+            this.fa,
+            this.proxy,
+            this.note});
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 241);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(435, 68);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // stt
+            // 
+            this.stt.HeaderText = "STT";
+            this.stt.Name = "stt";
+            // 
+            // email
+            // 
+            this.email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            // 
+            // pass
+            // 
+            this.pass.HeaderText = "PassWord";
+            this.pass.Name = "pass";
+            // 
+            // fa
+            // 
+            this.fa.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.fa.HeaderText = "2FA";
+            this.fa.Name = "fa";
+            // 
+            // proxy
+            // 
+            this.proxy.HeaderText = "Proxy";
+            this.proxy.Name = "proxy";
+            // 
+            // note
+            // 
+            this.note.HeaderText = "Ghi Chú";
+            this.note.Name = "note";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginEmailPassToolStripMenuItem,
+            this.loginEmailPass2FAToolStripMenuItem});
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // loginEmailPassToolStripMenuItem
+            // 
+            this.loginEmailPassToolStripMenuItem.Name = "loginEmailPassToolStripMenuItem";
+            this.loginEmailPassToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.loginEmailPassToolStripMenuItem.Text = "Login Email|Pass";
+            this.loginEmailPassToolStripMenuItem.Click += new System.EventHandler(this.loginEmailPassToolStripMenuItem_Click);
+            // 
+            // loginEmailPass2FAToolStripMenuItem
+            // 
+            this.loginEmailPass2FAToolStripMenuItem.Name = "loginEmailPass2FAToolStripMenuItem";
+            this.loginEmailPass2FAToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.loginEmailPass2FAToolStripMenuItem.Text = "Login Email|Pass|2FA";
+            this.loginEmailPass2FAToolStripMenuItem.Click += new System.EventHandler(this.loginEmailPass2FAToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 450);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.rdDataImport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rdDataImport;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pass;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn proxy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn note;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginEmailPassToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginEmailPass2FAToolStripMenuItem;
+    }
+}
+
---
+++ Login_Fb/Form1.cs
@@ -0,0 +1,258 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Fb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int nCurrentItem = 0; // xác định hàng đang được click
+
+        // Mình sẽ khai báo biến user, pass, 2fa để lúc chrome chạy lên có cái điền
+        public string sUser, sPasss, s2FA;
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            // Tách data thành các dòng riêng biệt
+            string[] lsInfo = rdDataImport.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            int nIndex = 0;
+            foreach(var detailInfo in lsInfo)
+            {
+                if(detailInfo == "")
+                {
+                    continue;
+                }
+
+                // Tách thành từng data như mail, pass, 2FA
+                string[] sInfo = detailInfo.Split(new[] { "|" }, StringSplitOptions.None); 
+
+                // Insert to datagirdview
+               if(sInfo.Length == 2) // data chứa user, pass
+                {
+                    dataGridView1.Rows.Insert(nIndex, nIndex + 1, sInfo[0], sInfo[1]);
+                }else if (sInfo.Length == 3) // data chứa user, passs, 2FA
+                {
+                    dataGridView1.Rows.Insert(nIndex, nIndex + 1, sInfo[0], sInfo[1], sInfo[2]);
+                }
+                else if (sInfo.Length == 4)// data chứa user, passs, 2FA, proxy
+                {
+                    dataGridView1.Rows.Insert(nIndex, nIndex + 1, sInfo[0], sInfo[1], sInfo[2], sInfo[3]);
+                }
+                else if (sInfo.Length == 5)// data chứa user, passs, 2FA, proxy, ghi chú
+                {
+                    dataGridView1.Rows.Insert(nIndex, nIndex + 1, sInfo[0], sInfo[1], sInfo[2], sInfo[3], sInfo[4]);
+                }
+                nIndex++;
+            }    
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Lưu lại vị trí người dùng mới click
+            nCurrentItem = e.RowIndex;
+        }
+
+        private void loginEmailPassToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Khi người dùng click login bằng user | pass
+
+            //  ==> Để tạo được hành vi này cài đặt chrome, selenium
+            // B1: khởi tạo chrome
+            ChromeDriverService driverService = null;
+            ChromeDriver driver = null;
+            IWebElement item = null;
+
+            driverService = ChromeDriverService.CreateDefaultService(".\\");
+            driverService.HideCommandPromptWindow = true;
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            chromeOptions.AddArguments(
+                "--headless",
+                "--verbose",
+                //"--incognito",
+                "--disable-web-security",
+                "--ignore-certificate-errors",
+                "--allow-running-insecure-content",
+                "--allow-insecure-localhost"
+                );
+
+            chromeOptions.AddUserProfilePreference("webrtc.ip_handling_policy", "disable_non_proxied_udp");
+            chromeOptions.AddUserProfilePreference("webrtc.multiple_routes_enabled", false);
+            chromeOptions.AddUserProfilePreference("webrtc.nonproxied_udp_enabled", false);
+            chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.geolocation", 1);
+
+            chromeOptions.AddArgument("--mute-audio");
+            chromeOptions.AddArgument("--disable-gpu");
+            chromeOptions.AddArgument("--disable-software-rasterizer");
+            chromeOptions.AddArgument("--no-first-run");
+            chromeOptions.AddArgument("--no-service-autorun");
+            chromeOptions.AddArgument("--password-store=basic");
+            chromeOptions.AddArgument("--disable-dev-shm-usage");
+            chromeOptions.AddArgument("--disable-blink-features=AutomationControlled");
+            chromeOptions.AddExcludedArgument("enable-automation");
+
+            driver = new ChromeDriver(driverService, chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            driver.Navigate().GoToUrl("https://m.facebook.com");
+
+            // B2: Tìm xpath hoặc id để điền email
+            item = driver.FindElement(By.Id("m_login_email"));
+            // B2.1: Ta lấy email từ chỗ click ra để điền, ta đã có vị trí dòng đang click, truy nhập dòng đó lấy ra thôi
+            sUser = dataGridView1.Rows[nCurrentItem].Cells[1].Value.ToString();  // => email là vị trí cột thứ 2 tại dòng ta đang click
+            // B2.2: Ta điền email vào phần email trên facebook
+            item.SendKeys(sUser);
+
+            // B3: Ta tìm chỗ điền password
+            item = driver.FindElement(By.Id("m_login_password"));
+            // B3.1: Ta lấy pass từ chỗ click ra để điền
+            sPasss = dataGridView1.Rows[nCurrentItem].Cells[2].Value.ToString();  // => pass là vị trí cột thứ 3 tại dòng ta đang click
+            item.SendKeys(sPasss);
+
+            // B4: Ta tìm button click để login
+            item = driver.FindElement(By.XPath("//*[@id=\"login_password_step_element\"]/button"));
+            item.Click();
+
+            // ==> Xong phần login bằng user | pass, ta chuyển sang phần login bằng 2FA
+        }
+
+        // Hoàn toàn tương tự, khi click có 2FA thì đến đoạn sau ta cần lấy code 2FA
+        private void loginEmailPass2FAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Khi người dùng click bằng user | pass| 2FA
+
+            // Khi người dùng click login bằng user | pass
+
+            //  ==> Để tạo được hành vi này cài đặt chrome, selenium
+            // B1: khởi tạo chrome
+            ChromeDriverService driverService = null;
+            ChromeDriver driver = null;
+            IWebElement item = null;
+
+            driverService = ChromeDriverService.CreateDefaultService("C:\\Users\\Admin\\source\\repos\\Login_Fb\\Login_Fb\\bin\\Debug");
+            driverService.HideCommandPromptWindow = true;
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            chromeOptions.AddArguments(
+                "--verbose",
+                //"--incognito",
+                "--disable-web-security",
+                "--ignore-certificate-errors",
+                "--allow-running-insecure-content",
+                "--allow-insecure-localhost"
+                );
+
+            chromeOptions.AddUserProfilePreference("webrtc.ip_handling_policy", "disable_non_proxied_udp");
+            chromeOptions.AddUserProfilePreference("webrtc.multiple_routes_enabled", false);
+            chromeOptions.AddUserProfilePreference("webrtc.nonproxied_udp_enabled", false);
+            chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.geolocation", 1);
+
+            chromeOptions.AddArgument("--mute-audio");
+            chromeOptions.AddArgument("--disable-gpu");
+            chromeOptions.AddArgument("--disable-software-rasterizer");
+            chromeOptions.AddArgument("--no-first-run");
+            chromeOptions.AddArgument("--no-service-autorun");
+            chromeOptions.AddArgument("--password-store=basic");
+            chromeOptions.AddArgument("--disable-dev-shm-usage");
+            chromeOptions.AddArgument("--disable-blink-features=AutomationControlled");
+            chromeOptions.AddExcludedArgument("enable-automation");
+
+            driver = new ChromeDriver(driverService, chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            driver.Navigate().GoToUrl("https://m.facebook.com");
+            Thread.Sleep(2000);
+
+            // B2: Tìm xpath hoặc id để điền email
+            item = driver.FindElement(By.Id("m_login_email"));
+            // B2.1: Ta lấy email từ chỗ click ra để điền, ta đã có vị trí dòng đang click, truy nhập dòng đó lấy ra thôi
+            sUser = dataGridView1.Rows[nCurrentItem].Cells[1].Value.ToString();  // => email là vị trí cột thứ 2 tại dòng ta đang click
+            // B2.2: Ta điền email vào phần email trên facebook
+            item.SendKeys(sUser);
+            Thread.Sleep(1000);
+
+            // B3: Ta tìm chỗ điền password
+            item = driver.FindElement(By.Id("m_login_password"));
+            // B3.1: Ta lấy pass từ chỗ click ra để điền
+            sPasss = dataGridView1.Rows[nCurrentItem].Cells[2].Value.ToString();  // => pass là vị trí cột thứ 3 tại dòng ta đang click
+            item.SendKeys(sPasss);
+            Thread.Sleep(1000);
+
+            // B4: Ta tìm button click để login
+            item = driver.FindElement(By.XPath("//*[@id=\"login_password_step_element\"]/button"));
+            item.Click();
+            Thread.Sleep(1000);
+
+            // ==> Xong phần login bằng user | pass, ta chuyển sang phần login bằng 2FA
+            string s2FA = dataGridView1.Rows[nCurrentItem].Cells[3].Value.ToString(); // => 2FA là vị trí cột thứ 4 tại dòng ta đang click
+            string s2FACode = GetCode(s2FA);
+
+            // B5: Ta tìm vị trí điền 2FA
+            item = driver.FindElement(By.Id("approvals_code"));
+            item.SendKeys(s2FACode);
+            Thread.Sleep(1000);
+
+            // B6: Ta click vào button 2FA
+            item = driver.FindElement(By.Id("checkpointSubmitButton-actual-button"));
+            item.SendKeys(s2FACode);
+            item.Click();
+
+            //==> Hoàn thành check code bằng 2FA
+        }
+
+        // Ta sẽ viết hàm lấy 2FA tại đây, đầu vào là chuổi 2FA, trả ra là code
+        public string GetCode(string s2FA)
+        {
+            string sCode = "";
+
+            var client = new RestClient("https://2fa.vn/server");
+            client.Timeout = 15000;
+            client.FollowRedirects = false;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("authority", "2fa.vn");
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+            request.AddHeader("accept-language", "en-US,en;q=0.9");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+            request.AddHeader("cookie", "PHPSESSID=uithlpksu7aqoobltpopk0opgc; _ga_L553VMWZW6=GS1.1.1667246078.1.0.1667246078.0.0.0; _ga=GA1.1.1818061002.1667246079");
+            request.AddHeader("origin", "https://2fa.vn");
+            request.AddHeader("referer", "https://2fa.vn/");
+            request.AddHeader("sec-ch-ua", "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"");
+            request.AddHeader("sec-ch-ua-mobile", "?0");
+            request.AddHeader("sec-ch-ua-platform", "\"Windows\"");
+            request.AddHeader("sec-fetch-dest", "empty");
+            request.AddHeader("sec-fetch-mode", "cors");
+            request.AddHeader("sec-fetch-site", "same-origin");
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36";
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            var body = @"secrets=%5B%22OO6Y57NT4IQEPPA6J7JIN4ORVRRY52GE%22%5D";
+            request.AddParameter("application/x-www-form-urlencoded; charset=UTF-8", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            // sau khi có kết quả trả về ta cần lọc ra cái code
+            string sResponse = response.Content.Substring(response.Content.IndexOf("code"));
+            sCode = Regex.Match(sResponse, @"\d+").Value;
+
+            // 2 dòng code trên là ta lấy ra mã code
+
+            return sCode;
+        }
+    }
+}
